64e9a11c8cd30b52d9d60e48c5e2c4f3b6bb72b3
kaosiso <kaosisochukwum@gmail.com>
2025-09-22 17:20:05 +0100

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ['latin'],
+  weight:['400', '700'],
+
+})
+
+
+export const metadata: Metadata = {
+  title: "FINANCIER",
+  description: "Financier is a mobile banking platform for everyone",
+  icons:{
+    icon: "/icons/logo.svg"
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} ${ibmPlexSerif.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
